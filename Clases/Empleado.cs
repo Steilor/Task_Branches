9d8090561a69ebddba549da44fd456a3a3c5ea38
Steilor <steilormateo.44@gmail.com>
2023-09-12 22:54:08 -0400

    Agregar archivos de proyecto.

---
+++ Clases/Empleado.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Clase.Clases
+{
+    public class Empleado:MiembrosDeLaComunidad
+    {
+        public new virtual void saludo()
+        {
+            // Agregar código personalizado al método sobrescrito
+            Console.WriteLine("Buenos días, empleados");
+        }
+    }
+}
---
+++ Clases/MiembrosDeLaComunidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase.Clases
+{
+    public abstract class MiembrosDeLaComunidad
+    {
+        public void saludo()
+        {
+            Console.WriteLine("Buenos dias Miembros de la comunidad");
+
+        }
+    }
+}
